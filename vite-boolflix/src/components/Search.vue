bfdbd24ecb326ea77fafdbd507aad5fdb6bfc61d
federicoalagna <federicoalagna01@gmail.com>
2024-06-08 10:22:59 +0200

    valutazione funzionante

--- vite-boolflix/src/components/Search.vue
+++ vite-boolflix/src/components/Search.vue
@@ -1,37 +1,41 @@
 <script>
-
-
 import { ref } from 'vue';
 import { store } from '../store';
 
 export default {
   setup() {
-
     const query = ref('');
-
 
     const search = () => {
       if (query.value.trim()) {
         store.searchMoviesAndTvShows(query.value);
       }
     };
-    const getFlagUrl = (languageCode) => {
-      return `/flags/it.svg${languageCode}.svg`;
+
+    const getMovieImageUrl = (path) => {
+      return path ? `https://image.tmdb.org/t/p/w342${path}` : 'path/to/default/image.jpg';
+    };
+
+    const getTvShowImageUrl = (path) => {
+      return path ? `https://image.tmdb.org/t/p/w342${path}` : 'path/to/default/image.jpg';
     };
 
     return {
       query,
       store,
       search,
-      getFlagUrl,
+      getMovieImageUrl,
+      getTvShowImageUrl,
     };
   },
 };
-
-
 </script>
 
+
+
+
 <template>
+      <img src="https://edu-boolflix.netlify.app/assets/img/logo.png" alt="Titolo" />
   <div>
     <!-- Barra di ricerca -->
     <input v-model="query" @keyup.enter="search" placeholder="Cerca un film o una serie tv..." />
@@ -42,58 +46,78 @@ export default {
     <div v-if="store.error">{{ store.error }}</div>
 
     <!-- Lista dei risultati per i film -->
-    <h2>Film</h2>
+    <div class="big">Film</div>
     <ul v-if="store.movies.length">
-      <li v-for="movie in store.movies" :key="movie.id">
-        <h3>{{ movie.title }} ({{ movie.original_title }})</h3>
-        <p>
-          Lingua: 
-          <img 
-            v-if="movie.original_language === 'it'" 
-            src="/flags/it.svg" 
-            alt="Italian Flag" 
-            class="flag" 
-          />
-          <img 
-            v-else-if="movie.original_language === 'en'" 
-            src="/flags/en.svg" 
-            alt="UK Flag" 
-            class="flag" 
-          />
-          <span v-else>
-            {{ movie.original_language }}
-          </span>
-        </p>
-        <p>Voto: {{ movie.vote_average }}</p>
-      </li>
-    </ul>
+  <li v-for="movie in store.movies" :key="movie.id">
+    <img :src="getMovieImageUrl(movie.poster_path)" alt="Copertina di {{ movie.title }}" class="poster" />
+    <div class="movie-details">
+      <h3>{{ movie.title }} ({{ movie.original_title }})</h3>
+      <p>
+        Lingua: 
+        <img 
+          v-if="movie.original_language === 'it'" 
+          src="/flags/it.svg" 
+          alt="Italian Flag" 
+          class="flag" 
+        />
+        <img 
+          v-else-if="movie.original_language === 'en'" 
+          src="/flags/en.svg" 
+          alt="UK Flag" 
+          class="flag" 
+        />
+        <span v-else>
+          {{ movie.original_language }}
+        </span>
+      </p>
+      <!-- condizione per verificare se "vote_average" è definito -->
+      <p v-if="movie.vote_average !== undefined" class="stars">
+        Voto:
+        <!-- Calcolo del numero di stelle piene -->
+        <i v-for="i in Math.ceil(movie.vote_average / 2)" class="fas fa-star"></i>
+        <!-- Calcolo del numero di stelle vuote -->
+        <i v-for="i in Math.ceil((10 - movie.vote_average) / 2)" class="far fa-star"></i>
+      </p>
+    </div>
+  </li>
+</ul>
 
     <!-- Lista dei risultati per le serie TV -->
-    <h2>Serie TV</h2>
+    <div class="big">Serie TV</div>
     <ul v-if="store.tvShows.length">
-      <li v-for="show in store.tvShows" :key="show.id">
-        <h3>{{ show.name }} ({{ show.original_name }})</h3>
-        <p>
-          Lingua: 
-          <img 
-            v-if="show.original_language === 'it'" 
-            src="/flags/it.svg" 
-            alt="Italian Flag" 
-            class="flag" 
-          />
-          <img 
-            v-else-if="show.original_language === 'en'" 
-            src="/flags/en.svg" 
-            alt="UK Flag" 
-            class="flag" 
-          />
-          <span v-else>
-            {{ show.original_language }}
-          </span>
-        </p>
-        <p>Voto: {{ show.vote_average }}</p>
-      </li>
-    </ul>
+  <li v-for="show in store.tvShows" :key="show.id">
+    <img :src="getTvShowImageUrl(show.poster_path)" alt="Copertina di {{ show.name }}" class="poster" />
+    <div class="tv-show-details">
+      <h3>{{ show.name }} ({{ show.original_name }})</h3>
+      <p>
+        Lingua: 
+        <img 
+          v-if="show.original_language === 'it'" 
+          src="/flags/it.svg" 
+          alt="Italian Flag" 
+          class="flag" 
+        />
+        <img 
+          v-else-if="show.original_language === 'en'" 
+          src="/flags/en.svg" 
+          alt="UK Flag" 
+          class="flag" 
+        />
+        <span v-else>
+          {{ show.original_language }}
+        </span>
+      </p>
+  
+      <p v-if="show.vote_average !== undefined" class="stars">
+        Voto:
+        <!-- Calcolo del numero di stelle piene -->
+        <i v-for="i in Math.ceil(show.vote_average / 2)" class="fas fa-star"></i>
+        <!-- Calcolo del numero di stelle vuote -->
+        <i v-for="i in Math.ceil((10 - show.vote_average) / 2)" class="far fa-star"></i>
+      </p>
+    </div>
+  </li>
+</ul>
   </div>
 </template>
 
@@ -116,10 +140,23 @@ ul {
 
 li {
   margin-bottom: 16px;
+  display: flex;
+  align-items: center;
 }
+
+h3 {
+  margin: 0 0 8px 0;
+}
+
+.poster {
+  width: 100px; 
+  margin-right: 16px;
+}
+
 .flag {
   width: 24px;
   height: auto; 
   display: inline-block;
+  margin-left: 4px;
 }
 </style>
--- vite-boolflix/src/store.js
+++ vite-boolflix/src/store.js
@@ -5,31 +5,28 @@ import axios from 'axios';
 
 const apiKey = '2c8f6c0253ae3dd944baa8b55c4f808f';
 
+
 export const store = reactive({
   movies: [],
   tvShows: [],
   loading: false,
   error: null,
-  searchMoviesAndTvShows(query) {
-    const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&language=it-IT`;
-    const tvUrl = `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${query}&language=it-IT`;
 
+  async searchMoviesAndTvShows(query) {
     this.loading = true;
     this.error = null;
-    this.movies = [];
-    this.tvShows = [];
+    try {
+      const [moviesResponse, tvShowsResponse] = await Promise.all([
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&language=it-IT`),
+        axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${query}&language=it-IT`)
+      ]);
 
-    // Effettua entrambe le chiamate API in parallelo
-    Promise.all([axios.get(movieUrl), axios.get(tvUrl)])
-      .then(([movieResponse, tvResponse]) => {
-        this.movies = movieResponse.data.results;
-        this.tvShows = tvResponse.data.results;
-      })
-      .catch(err => {
-        this.error = 'Errore durante la ricerca dei film e delle serie TV';
-      })
-      .finally(() => {
-        this.loading = false;
-      });
-  },
+      this.movies = moviesResponse.data.results;
+      this.tvShows = tvShowsResponse.data.results;
+    } catch (error) {
+      this.error = 'Errore durante la ricerca. Riprova più tardi.';
+    } finally {
+      this.loading = false;
+    }
+  }
 });
